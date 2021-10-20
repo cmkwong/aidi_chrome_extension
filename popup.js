c387d8d63b73dc2adadab69764c37b736c0acc83
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-10-20 16:48:17 +0800

    click to change the active-tag background color

--- popup.js
+++ popup.js
@@ -1,5 +1,9 @@
 let changeColor = document.getElementById("changeColor");
 
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
 changeColor.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
