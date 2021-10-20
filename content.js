ee45eebce1062d6c0e3ae4cbbb29c49536ed93fd
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-10-20 23:48:44 +0800

    added to the function onclick instead addEventListener()

--- content.js
+++ content.js
@@ -2,43 +2,28 @@ let time_fn = setInterval(() => {
   // First
   let project_name_el = document.querySelector("#project-name");
   if (project_name_el) {
-    if (project_name_el.getAttribute("listener") !== "true") {
-      project_name_el.addEventListener(
-        "mouseenter",
-        () => {
-          console.log(document.querySelector("#project-name").innerText.trim());
-          project_name_el.setAttribute("listener", "true");
-        },
-        { once: true }
-      );
+    if (!project_name_el.onclick) {
+      project_name_el.onclick = () => {
+        console.log(document.querySelector("#project-name").innerText.trim());
+      };
     }
   }
   // Second
   let message_el = document.querySelector(".message");
   if (message_el) {
-    if (message_el.getAttribute("listener") !== "true") {
-      message_el.addEventListener(
-        "click",
-        () => {
-          console.log("abc here");
-          message_el.setAttribute("listener", "true");
-        },
-        { once: true }
-      );
+    if (!message_el.onclick) {
+      message_el.onclick = () => {
+        console.log("abc here");
+      };
     }
   }
   // Third
   let title_el = document.querySelector(".header-wrapper");
   if (title_el) {
-    if (title_el.getAttribute("listener") !== "true") {
-      title_el.addEventListener(
-        "click",
-        () => {
-          console.log("asasdasdasdasdasdasdasdasdasda");
-          title_el.setAttribute("listener", "true");
-        },
-        { once: true }
-      );
+    if (!title_el.onclick) {
+      title_el.onclick = () => {
+        console.log("asasdasdasdasdasdasdasdasdasda");
+      };
     }
   }
 }, 1000);
