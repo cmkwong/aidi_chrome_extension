7a9f88fb943209e19f8b8d1a78cb3f0f3b26be7c
cmkwong <cheung.mankwong@yahoo.com.hk>
2021-10-20 22:53:46 +0800

    added the message listener for CC

--- content.js
+++ content.js
@@ -3,3 +3,8 @@ let title_el = document.querySelector("#header-wrapper");
 title_el.addEventListener("click", () => {
   console.log(title_el.innerText);
 });
+
+let message_el = document.querySelector(".message");
+message_el.addEventListener("click", () => {
+  console.log("message clicked");
+});
